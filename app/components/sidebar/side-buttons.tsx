5f90e53ffcf8fa521c3bf8d03413298e61ca517a
MuratOfficial <toimetm@gmail.com>
2025-05-15 20:21:31 +0500

    test

--- app/components/sidebar/side-buttons.tsx
+++ app/components/sidebar/side-buttons.tsx
@@ -12,7 +12,7 @@ interface SideButtonsProps {
 
 function SideButtons({href="/", title, icon, img, classname}:SideButtonsProps) {
   return (
-    <Link href={href} className={'w-9 h-9 hover:bg-blue-500 transition delay-75 duration-200 rounded-lg flex flex-row items-center justify-center text-center '+ classname}>
+    <Link href={href} className={'w-10 h-10 hover:bg-blue-500 transition delay-75 duration-200 rounded-xl flex flex-row items-center justify-center text-center '+ classname}>
         {icon ? icon : img ? <img src={img} alt={title} className=' object-fill w-full h-full'/>:<p className='text-base font-medium text-white'>{title[0].toUpperCase()}</p>}
    
     
